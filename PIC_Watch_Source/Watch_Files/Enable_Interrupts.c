25f6a18cd1d52ff3c468aefb1ec7fbb18b0248cb
Timothy M <124601612+TimothyMarchant@users.noreply.github.com>
2024-08-15 17:11:48 -0400

    Update Enable_Interrupts.c

--- PIC_Watch_Source/Watch_Files/Enable_Interrupts.c
+++ PIC_Watch_Source/Watch_Files/Enable_Interrupts.c
@@ -1,27 +1,24 @@
 #include <xc.h>
 unsigned char timer1cnt = 0;
+#define ButtonINTPIN 0x3C
+#define AlarmINTPIN 0x04
+#define AlarmINTPIN_TRI TRISA2
 void IOC(void){
-    IOCAN=0x00;
-    IOCCP = 0x3C;
+    //button interrupt pins.
+    IOCCP = ButtonINTPIN;
+    //RA2 alarm interrupt pin.
+    IOCAN2=1;
+    IOCAF2=0;
+    AlarmINTPIN_TRI=1;
     IOCIE = 1;
-    
-    //RA1
-    //IOCAN=0x02;
-}
-void Timer0(void){
-    
 }
 //the exact time of timer1 is not time critical.  It will simply be used as a timeout for I2C.
-//timer increments only on multiples of FOSC (Hfintz based on the prescaler.  The clock rate is 32MHz
-//Then the time for a single increment to the timer is FOSC/prescaler*4
-//for timer1=0 to 0xFFFF then the delay is equal to (FOSC/prescaler*4)*0xFFFF.
-//we need only to make the delay long enough to really make sure the bus is hanging.
 void Timer1(void){
     TMR1IE=1;
     //1:4 prescaler. Asychronous.
     T1CON=0x26;
     T1GCONbits.GE=0;
-    //FOSC/4. for now.
+    //FOSC/4 clock source.
     T1CLKbits.CS=0x01;
     TMR1=0;
 }
@@ -36,7 +33,6 @@ unsigned char GetTimer1cnt(void){
 }
 void EnableInterrupts(void){
     IOC();
-    //Timer0();
     Timer1();
     PEIE = 1;
     GIE = 1;
