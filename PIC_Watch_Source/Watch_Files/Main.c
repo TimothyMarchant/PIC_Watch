088c688304e518fa703d7b2c9bb87016b96c073e
Timothy M <124601612+TimothyMarchant@users.noreply.github.com>
2024-08-15 17:13:41 -0400

    Update Main.c

--- PIC_Watch_Source/Watch_Files/Main.c
+++ PIC_Watch_Source/Watch_Files/Main.c
@@ -3,13 +3,12 @@
 #include "SSD1306_Drivers.h"
 #include "MCP9700_Drivers.h"
 #include "MCP7940_Drivers.h"
-/* This project uses the MCP7940M RTC, SSD1306, and the MCP9700 analog temperature sensor
+/* This project uses the MCP7940N RTC, SSD1306, and the MCP9700 analog temperature sensor
  * This microcontroller was picked for several reasons
  * 1st, I had these in my house already and seemed to be big enough to do the job.
  * 2nd it's small enough for a <1.5 inch PCB (need it to be smaller than this more than likely; the PCB that is).
  * 
  */
-//
 #ifndef _XTAL_FREQ
 #define _XTAL_FREQ 32000000
 #endif
@@ -17,6 +16,7 @@
 #define Button2 PORTCbits.RC3
 #define Button3 PORTCbits.RC4
 #define Button4 PORTCbits.RC5
+#define INTDATA 0xFF
 #define PORTCButtons 0b00111100
 void SendNumber(unsigned int);
 void SendByte(unsigned char);
@@ -28,12 +28,28 @@ unsigned int timer0cnt = 0;
 unsigned char data = 0;
 _Bool DeepSleep = 0;
 _Bool IsPressed = 0;
+_Bool NeedToUpdateTime=0;
 void GotoSleep(void);
 void DisplayTime(void);
 void DisplayState(void);
+void UpdateTimeArray(void);
+void UpdateDateArray(void);
+void UpdateYearArray(void);
 void __interrupt() ISR(void) {
+    //I2C recovery
+    if (TMR1IF) {
+        TMR1ON=0;
+        IncrementTimer1cnt();
+        TMR1IF=0;
+        //timeout has clearly gone on too long restart the device after sending 9 clock pulses.
+        if (GetTimer1cnt()==100){
+            ForceReset();
+        }
+        TMR1=0;
+        TMR1ON=1;
+    }
     //wakeup device.
-    if (IOCCF) {
+    if (IOCCF>0) {
         //for debouncing
         __delay_ms(50);
         //get inputs from RC2-RC5 only.
@@ -41,23 +57,19 @@ void __interrupt() ISR(void) {
         IOCCF = 0;
         IOCIF = 0;
     }
-    if (TMR0IF) {
-
-    }
-    //I2C recovery
-    if (TMR1IF) {
-        TMR1ON=0;
-        IncrementTimer1cnt();
-        TMR1IF=0;
-        if (GetTimer1cnt()==100){
-            
-            ForceReset();
-        }
-        TMR1=0;
-        TMR1ON=1;
+    //alarm interrupt
+    if (IOCAF>0){
+        NeedToUpdateTime=1;
+        //we need not update anything here, as the alarm pin will not change states until we modify the internal registers of the RTC.
+        //The compiler gave a warning when we call such methods saying possible stack overflow.
+        IOCIF=0;
+        IOCAF=0;
+        data=0xFF;
     }
 }
+//this is for other files so that it can be changed elsewhere.
 void GotoSleep(void){
+    //
     DeepSleep=1;
     //do some other things.
 }
@@ -65,12 +77,14 @@ void main(void) {
     TRISA = 0x00;
     LATA = 0x00;
     TRISA0=1;
-    ANSELAbits.ANSA0 = 1;
+    ANSELA=0x01;
+    TRISA2=1;
     LATC = 0x00;
     //RC2-RC5 are all inputs.
     TRISC = 0xFF;
     ANSELC = 0x00;
     WPUC = 0x00;
+    WPUA=0x00;
     Temp_Init(&LATA, 5);
     EnableInterrupts();
     I2C_Init();
@@ -82,13 +96,23 @@ void main(void) {
         HandleButton(data);
         if (DeepSleep) {
             DisplayTurnOff();
+            //turn off alarm0 no interrupts are made in deep sleep.
+            TurnOffInterrupt();
         }
         SLEEP();
+        //We need this here instead of the ISR to avoid stack overflow.  The stack can only go 15 deep according to the datasheet (the compiler made a warning about it).
+        
         //turn the screen back on.
         if (DeepSleep) {
             DisplayTurnOn();
             DeepSleep = 0;
         }
-        
+        if (NeedToUpdateTime){
+            //update alarm time.
+            updatealarm0();
+            data=INTDATA;
+            DeepSleep=0;
+            NeedToUpdateTime=0;
+        }
     }
 }
